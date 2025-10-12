7b5f782712b65d2cb4e774940ba681d1e521c9d4
Ekaterina Chernyshova <newfatto@gmail.com>
2025-10-12 18:00:44 +0300

    Создана Админка и зарегистрированы модели

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_filter = ('name',)
+    search_fields = ('name', 'description')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name','category', 'price',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
+
