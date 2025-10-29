c30303f1f386c3a35a3b94ed31c6008c9d47012c
Ekaterina Chernyshova <newfatto@gmail.com>
2025-10-29 11:04:31 +0300

    Прописано содержимое всех шаблонов

--- blog/urls.py
+++ blog/urls.py
@@ -6,9 +6,9 @@ from blog.views import ArticleListView, ArticleDetailView, ArticleCreateView, Ar
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path("blog/", ArticleListView.as_view(), name="blog"),
-    path("blog/new/", ArticleCreateView.as_view(), name='new_article'),
-    path("blog/<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
-    path("blog/<int:pk>/edit/", ArticleUpdateView.as_view(), name='article_edit'),
-    path("blog/<int:pk>/confirm_delete", ArticleDeleteView.as_view(), name='article_delete')
+    path("", ArticleListView.as_view(), name="blog"),
+    path("new/", ArticleCreateView.as_view(), name='new_article'),
+    path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
+    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name='article_edit'),
+    path("<int:pk>/confirm_delete/", ArticleDeleteView.as_view(), name='article_delete')
 ]
