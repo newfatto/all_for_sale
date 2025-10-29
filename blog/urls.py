7ca3c790bad90d58889cf018560ee7c17c20a20e
Ekaterina Chernyshova <newfatto@gmail.com>
2025-10-29 10:18:05 +0300

    Прописаны urls приложения blog и созданы файлы шаблонов

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-# from blog.views import ArticleListView, ArticleDetailView, ArticleCreateView
+from blog.views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleDeleteView, ArticleUpdateView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    # path("blog/", ArticleListView.as_view(), name="home"),
-    # path("blog/<int:pk>/", ArticleDetailView.as_view(), name="article"),
-    # path("blog/<int:pk>/edit", ArticleCreateView.as_view(), name='article_edit'),
+    path("blog/", ArticleListView.as_view(), name="blog"),
+    path("blog/new/", ArticleCreateView.as_view(), name='new_article'),
+    path("blog/<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
+    path("blog/<int:pk>/edit/", ArticleUpdateView.as_view(), name='article_edit'),
+    path("blog/<int:pk>/confirm_delete", ArticleDeleteView.as_view(), name='article_delete')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from blog.models import Article
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'blog.html'
+    context_object_name = 'articles'
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_edit.html'
+    fields = ['title', 'content', 'preview', ]
+    success_url = reverse_lazy('blog:blog')
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+    context_object_name = 'article'
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ['title', 'content', 'preview', ]
+    template_name = 'article_edit.html'
+    success_url = reverse_lazy('blog:blog')
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_confirm_delete.html'
+    success_url = reverse_lazy('blog:blog')
