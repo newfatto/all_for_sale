0426e262e4cae400341a05a600cf5bd4cd15a398
Ekaterina Chernyshova <newfatto@gmail.com>
2025-11-21 20:24:55 +0300

    Добавлена группа контент-менеджеров. Действия с контентом блога (создание, редактирование, удаление статей) доступны только этй группе. Внесены соответствующие правки в контроллеры и шаблоны

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse, reverse_lazy
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
@@ -11,11 +12,12 @@ class ArticleListView(ListView):
     context_object_name = "articles"
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Article
     template_name = "article_edit.html"
     fields = ["title", "content", "preview", "is_published"]
     success_url = reverse_lazy("blog:blog")
+    permission_required = "blog.add_article"
 
 
 class ArticleDetailView(DetailView):
@@ -30,16 +32,18 @@ class ArticleDetailView(DetailView):
         return obj
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Article
     fields = ["title", "content", "preview", "is_published"]
     template_name = "article_edit.html"
+    permission_required = "blog.change_article"
 
     def get_success_url(self):
         return reverse("blog:article_detail", args=[self.kwargs.get("pk")])
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Article
     template_name = "article_confirm_delete.html"
     success_url = reverse_lazy("blog:blog")
+    permission_required = "blog.delete_article"
